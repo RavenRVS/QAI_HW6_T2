2162927e09080a97944ff1e2d97c9c0ad9a8354d
Vitaly Remnev <remnevvs@mail.ru>
2022-08-24 19:05:34 +0300

    Added resources/data.csv

---
+++ src/test/java/ru/netology/javaqa/javaqai/Task2/services/BonusServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.javaqa.javaqai.Task2.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources="/data.csv")
+    public void testRegisteredUnderLimit(int expected, int amount, boolean isRegistered) {
+        BonusService service = new BonusService();
+
+        int actual = service.calcBonus(amount, isRegistered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
